8e0cde0a8c2f5d35797e5c21c4ba7dcf2a42afc0
KO-SEOKJUN <tjrwns1021@gmail.com>
2021-03-22 22:48:14 +0900

    my fisrt swift study

---
+++ 2021.개인공부/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 고석준 on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+}
+
