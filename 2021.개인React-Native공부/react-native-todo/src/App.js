1c3df4901465d27390c25ca59fc596fd4092b828
SEOKJUN KO <tjrwns1021@gmail.com>
2021-08-22 19:44:26 +0900

    react-native todo update 2021-8-22

--- 2021.개인React-Native공부/react-native-todo/src/App.js
+++ 2021.개인React-Native공부/react-native-todo/src/App.js
@@ -1,36 +1,73 @@
-import React from 'react';
-import styled, {ThemeProvider} from 'styled-components/native';
-import {theme} from './theme';
-import { StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, Dimensions } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
 import Input from './components/Input';
+import Task from './components/Task';
 
 const Container = styled.SafeAreaView`
-    flex: 1;
-    background-color: ${({theme}) => theme.background};
-    align-items: center;
-    justify-content: flex-start;
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 const Title = styled.Text`
-    font-size: 40px;
-    font-weight: 600;
-    color: ${({theme}) => theme.main};
-    align-self: flex-start;
-    margin: 0px 20px;
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.main};
+  align-self: flex-start;
+  margin: 20px;
 `;
 
-const App = () => {
-    return(
-        <ThemeProvider theme={theme}>
-            <Container>
-                <StatusBar 
-                    barStyle="light-content"
-                    backgroundColor={theme.background}/>
-                <Title> TODO List</Title>
-                <Input placeholder="+ Add a Task"/>
-            </Container>
-        </ThemeProvider>
-    );
-};
+const List = styled.ScrollView`
+  flex: 1;
+  width: ${({ width }) => width - 40}px;
+`;
 
-export default App;
+export default function App() {
+  const width = Dimensions.get('window').width;
+
+  const [tasks, setTask] = useState({
+    '1': { id: '1', text: 'Korea', completed: false},
+    '2': { id: '2', text: 'React', completed: true},
+    '3': { id: '3', text: 'Native', completed: false},
+    '4': { id: '4', text: 'Edit', completed: false},
+  });
+
+  const [newTask, setNewTask] = useState('');
+
+  const _addTask = () => {
+    alert(`Add: ${newTask}`);
+    setNewTask('');
+  };
+
+  const _handleTextChange = text => {
+    setNewTask(text);
+  };
+
+  return(
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.background} // Android only
+        />
+        <Title>TODO List</Title>
+        <Input
+          placeholder="+ Add a Task"
+          value={newTask}
+          onChangeText={_handleTextChange}
+          onSubmitEditing={_addTask}
+        />
+        <List width={width}>
+          {Object.values(tasks)
+            .reverse()
+            .map(item => (
+              <Task key={item.id} text={item.text} />
+            ))}
+        </List>
+      </Container>
+    </ThemeProvider> 
+  );
+}
--- 2021.개인React-Native공부/react-native-todo/src/components/Input.js
+++ 2021.개인React-Native공부/react-native-todo/src/components/Input.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
 
-const StyledInput = styled.TextInput`
-    background-color: ${({theme}) => theme.itemBackground};
-    width: ${({width}) => width - 40}px;
-    height: 60px;
-    margin: 3px 0;
-    padding: 15px 20px;
-    border-radius: 10px;
-    font-size: 25px;
-    color: ${({theme}) => theme.text};
+const StyledInput = styled.TextInput.attrs(({ theme }) => ({
+  placeholderTextColor: theme.main,
+}))`
+  width: ${({ width }) => width - 40}px;
+  height: 60px;
+  margin: 3px 0;
+  padding: 15px 20px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.itemBackground};
+  font-size: 25px;
+  color: ${({ theme }) => theme.text};
 `;
 
-const Input = ({placeholder}) => {
-    const width = Dimensions.get('window').width;
-    
-    return (<StyledInput width={width} placeholder={placeholder}></StyledInput>);
+const Input = ({
+  placeholder,
+  value,
+  onChangeText,
+  onSubmitEditing,
+}) => {
+  const width = Dimensions.get('window').width;
+  // const width = useWindowDimensions().width;
+
+  return (
+    <StyledInput
+      width={width}
+      placeholder={placeholder}
+      maxLength={50}
+      autoCapitalize="none"
+      autoCorrect={false}
+      returnKeyType="done"
+      keyboardAppearance="dark" // iOS only
+      value={value}
+      onChangeText={onChangeText}
+      onSubmitEditing={onSubmitEditing}
+    />
+  );
+};
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  onSubmitEditing: PropTypes.func.isRequired,
 };
 
 export default Input;
